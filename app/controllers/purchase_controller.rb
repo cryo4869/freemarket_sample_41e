861b9f4fa01f350501bb93695feea3c3686ad3df
yukihayashi <mrchildreny@gmail.com>
2019-04-30 02:30:21 +0900

    create purchase function

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,16 +1,17 @@
 class PurchaseController < ApplicationController
-  def index
+  def new
+    @product = Product.find(params[:id])
     card = Card.where(user_id: current_user.id).first
     #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
     if card.blank?
       #登録された情報がない場合にカード登録画面に移動
-      redirect_to controller: "card", action: "new"
+      redirect_to controller: "users", action: "card_create"
     else
       Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
       #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(card.customer_id)
       #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      @credit = customer.cards.retrieve(card.card_id)
     end
   end
   def pay
@@ -23,5 +24,8 @@ class PurchaseController < ApplicationController
       :currency => 'jpy', #日本円
       )
     redirect_to action: 'done' #完了画面に移動
+  end
+  def done
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
+  #post "/purchase/(:id)/(pay)",to: "purchase#pay"
+  resources :purchase, only: :index do
+    member do
+      post :new, to: "purchase#pay"
+      get :new
+      get :done
+    end
+  end
   resources :users do
     collection do
       get :signup
@@ -17,15 +25,6 @@ Rails.application.routes.draw do
     get 'category_bottom', on: :collection
     get 'search', on: :collection
   end
-<<<<<<< HEAD
   resources :buys
-  resources :purchase do
-    member do
-      post :pay
-    end
-=======
-  resources :buys do
-    get :purchase, on: :member
->>>>>>> master
-  end
+
 end
