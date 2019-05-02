fb924c936fd78c7c6af5962ca45289f99e75a579
yukihayashi <mrchildreny@gmail.com>
2019-05-02 14:58:32 +0900

    modify helper sell_status

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,7 +2,7 @@ module ProductsHelper
   def sell_status(status_id)
   case status_id
   when 2 then
-     return 'sell-stop' # オブジェクトが値1の時に実行される
+     return 'sell-stop'
   when 3 then
     return 'sell-sold'
   end
